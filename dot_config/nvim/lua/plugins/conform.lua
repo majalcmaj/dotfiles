cd8052be010fae3f5c56bdff927940ef5edf1a74
Michal Ciesielski <mciesielski2@atlassian.com>
2026-05-27 00:53:38 +0200

    Update .config/nvim/lua/plugins/conform.lua

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return { -- Autoformat
 			lua = { "stylua" },
 			python = { "isort", "black" },
 			json = { "jq" },
+			sql = { "sql-formatter" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
