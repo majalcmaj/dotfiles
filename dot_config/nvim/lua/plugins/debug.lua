454aa708009681c2c0b4b23f50dd44b483a5354b
Michał Ciesielski <majalcmaj@gmail.com>
2026-06-16 01:01:41 +0200

    Update .config/nvim/lua/plugins/debug.lua

--- dot_config/nvim/lua/plugins/debug.lua
+++ dot_config/nvim/lua/plugins/debug.lua
@@ -7,9 +7,7 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-	-- NOTE: Yes, you can install new plugins here!
 	"mfussenegger/nvim-dap",
-	-- NOTE: And you can specify dependencies as well
 	dependencies = {
 		-- Creates a beautiful debugger UI
 		"rcarriga/nvim-dap-ui",
@@ -26,56 +24,62 @@ return {
 		"mfussenegger/nvim-dap-python",
 	},
 	keys = {
-		-- Basic debugging keymaps, feel free to change to your liking!
 		{
-			"<F5>",
+			"<leader>ds",
 			function()
 				require("dap").continue()
 			end,
 			desc = "Debug: Start/Continue",
 		},
 		{
-			"<F1>",
+			"<leader>dt",
 			function()
-				require("dap").step_into()
+				require("dap").test_method()
 			end,
-			desc = "Debug: Step Into",
+			desc = "Debug: test",
 		},
 		{
-			"<F2>",
-			function()
-				require("dap").step_over()
-			end,
-			desc = "Debug: Step Over",
-		},
-		{
-			"<F3>",
-			function()
-				require("dap").step_out()
-			end,
-			desc = "Debug: Step Out",
-		},
-		{
-			"<leader>b",
+			"<leader>db",
 			function()
 				require("dap").toggle_breakpoint()
 			end,
 			desc = "Debug: Toggle Breakpoint",
 		},
 		{
-			"<leader>B",
+			"<leader>dB",
 			function()
 				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
 			end,
 			desc = "Debug: Set Breakpoint",
 		},
+		{
+			"<leader>di",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Debug: Step Into",
+		},
+		{
+			"<leader>do",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Debug: Step Over",
+		},
+		{
+			"<leader>dO",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Debug: Step Out",
+		},
 		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
 		{
-			"<F7>",
+			"<leader>dS",
 			function()
 				require("dapui").toggle()
 			end,
-			desc = "Debug: See last session result.",
+			desc = "Debug: See last session result. (Use when unhandled exception)",
 		},
 	},
 	config = function()
